790a5345e79bdf9d0f706e60a1cc051851446adb
Marcus Soares <111324245+MarcusSoaresCarioca@users.noreply.github.com>
2022-10-11 22:19:52 -0300

    criando funções

--- js/forca.js
+++ js/forca.js
@@ -10,8 +10,10 @@
 //palavras[palavras.length] = "Finlandia";
 //console.log(palavras[2]);
 
+let baseDados;
 let  pais;
-let continete;
+let continente;
+let sorteio;
 let url = "https://raw.githubusercontent.com/luannsr12/ddi-json-flag/main/data.json";
 const request = new XMLHttpRequest;
 request.open("GET", url); // abro a pagina
@@ -19,5 +21,28 @@ request.responseType = 'json'; //idico o tipo de retorno
 request.send(); //faço a leitura da pagina
 
 request.onload = function(){
-    
+    //Guardo o conteudo do arquivo na variavel
+    baseDados = request.response;
+    //console.log(baseDados[30]);
+    var totalPaises = Object.keys(baseDados).length;
+    //console.log(totalPaises);
+    sortearPais(totalPaises);
 }
+
+function sortearPais(total){
+    sorteio =Math.floor(Math.random() *  total);
+   // console.log(baseDados[sorteio]);
+    pais = baseDados[sorteio].pais;
+    continente = baseDados[sorteio].continente
+    //console.log(continente,pais);
+}
+
+function alfabeto(){
+    var botoes = '';
+    for(var i = 65; i <= 90; i++){
+        botoes += `<button class="btn btn-warning btn-lg font-monospace m-1 botao">${String.fromCharCode(i)}</button>`;
+    }
+    document.getElementById("alfabeto").innerHTML = botoes;
+}
+
+alfabeto();
